554dd0633c2a7abee957404faa52abe31f67667d
daylimbic <sidhys1@gmail.com>
2022-04-09 11:29:38 -0400

    Update ui.cpp

--- ui/Src/ui.cpp
+++ ui/Src/ui.cpp
@@ -1,17 +1,26 @@
 #include <GL/glew.h> /* order of including matters*/
 #include <GLFW/glfw3.h>
-#include <windows.h>
+#include <iostream>
+//#include <windows.h> {READ NOTE AT wWinMain}
+
+void GSWarn() {
+    std::cout << std::endl;
+}
+template<typename FirstGsWarn, typename ... RestGsWarn>
+void GSWarn(FirstGsWarn arg, const RestGsWarn&... rest) {
+    std::cout << arg << " ";
+    GSWarn(rest...);
+}
 
 
-int main()
+int __cdecl main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
+
     if (!glfwInit())
         return -1;
-
-    glewInit();
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -23,6 +32,9 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        GSWarn("[UI Init] glewInit() failed with status", glewInit());
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -37,12 +49,13 @@ int main()
         glfwPollEvents();
     }
 
+
     glfwTerminate();
     return 0;
 }
 
 /* removes console window [NOTE: change SUBSYSTEM in linker->system
-to not set while i figure this out */
+to not set while i figure this out 
 #ifdef WIN32
 int WINAPI wWinMain(
     __in HINSTANCE hInstance,
@@ -53,3 +66,4 @@ int WINAPI wWinMain(
     main();
 }
 #endif
+*/
